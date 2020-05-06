76e84fbdbfc0be061f13c15f591fc5ba882dc3ee
北間匠 <takumi_mac@kitakantakuminoMacBook-Pro.local>
2020-05-06 15:35:47 +0900

    [Add]キー情報の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,3 +8,5 @@ class ApplicationController < ActionController::Base
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
 	end
 end
+
+Refile.secret_key = '19a99e6f04ce0c26f335f63a880813d99c4169abfd36ca0acf17edb4cd5d9d885d6e29ab1bcbe7e5c68ae1b6e0820c14a48d10fcd8dff688078a35f2da6b6bf0'
